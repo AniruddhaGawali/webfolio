b3c67d9f5eb9a8f2fe87e08b7d5d58e0ee20371f
akg007 <aniruddhagawali03@gmail.com>
2023-10-01 23:12:42 +0530

    added favcion

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,12 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "The Dev Daily Bugle",
+  icons: {
+    icon: ["/favicon.ico"],
+    apple: ["/apple-touch-icon.png"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
