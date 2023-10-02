ea18f64035f91d8735a95d521d54b007d07fe774
akg007 <aniruddhagawali03@gmail.com>
2023-10-02 14:01:39 +0530

    update startup to index

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
-var cors = require("cors");
+const express = require('express');
+var cors = require('cors');
+const connect = require('./config/db');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  next();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.use("/api/intro", require("./controller/intro.controller"));
-app.use("/api/project", require("./controller/project.controller"));
-app.use("/api/skills", require("./controller/skills.controller"));
-app.use("/api/timeline", require("./controller/timeline.controller"));
-app.use("/api/contactme", require("./controller/contactme.controller"));
-app.use("/api/blog", require("./controller/blog.controller"));
+app.use('/api/intro', require('./controller/intro.controller'));
+app.use('/api/project', require('./controller/project.controller'));
+app.use('/api/skills', require('./controller/skills.controller'));
+app.use('/api/timeline', require('./controller/timeline.controller'));
+app.use('/api/contactme', require('./controller/contactme.controller'));
+app.use('/api/blog', require('./controller/blog.controller'));
 
-module.exports = app;
+app.listen(PORT, async () => {
+  await connect();
+  console.log(`Listening on the port ${PORT}`);
+});
+
+// module.exports = app;
