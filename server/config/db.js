4d913ecf1961a1bd4d5aea7af220cef711719e58
akg007 <aniruddhagawali03@gmail.com>
2023-10-02 15:49:52 +0530

    try to connent

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 dotenv.config();
 
-const connect = () => {
-  return mongoose.connect(process.env.MONGO_URI);
+const connect = async () => {
+  return await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connect;
