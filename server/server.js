d9791bddebe794b194ed482642b6f8e37e515404
akg007 <aniruddhagawali03@gmail.com>
2023-10-02 14:54:07 +0530

    fix : remove server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const app = require("./index");
-const connect = require("./config/db");
-const cors = require("cors");
+// const app = require("./index");
+// const connect = require("./config/db");
+// const cors = require("cors");
 
-const PORT = process.env.PORT || 5500;
+// const PORT = process.env.PORT || 5500;
 
-// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-app.use(cors());
+// // app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+// app.use(cors());
 
-app.listen(PORT, async () => {
-  await connect();
-  console.log(`Listening on the port ${PORT}`);
-});
+// app.listen(PORT, async () => {
+//   await connect();
+//   console.log(`Listening on the port ${PORT}`);
+// });
