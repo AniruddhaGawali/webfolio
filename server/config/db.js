4cc7362d361962e43c2d94fd01438e42d1917b8a
Aniruddha Gawali <73152642+AniruddhaGawali@users.noreply.github.com>
2025-02-09 02:39:28 +0530

    Remove unused configuration and model files

--- server/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const connect = async () => {
-  return await mongoose.connect(process.env.MONGO_URI);
-};
-
-module.exports = connect;
